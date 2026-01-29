07c58621e9bd54414b27a0b4e0e6f2fadd4896a7
Sofia <sofizam2006@gmail.com>
2026-01-29 12:36:11 +0100

    nuevo

--- src/saludo.php
+++ src/saludo.php
@@ -2,13 +2,26 @@
 
 function generarSaludo(string $nombre): string
 {
-    $saludos = [
-        "¡Hola $nombre! Gracias por visitar mi página 💜",
-        "¡Bienvenida $nombre! Qué alegría verte por aquí 😊",
-        "¡Hey $nombre! Espero que tengas un día genial ✨",
-        "¡Hola $nombre! Me encanta que estés aquí 🐶💜",
-        "¡Saludos $nombre! Disfruta la página 🌸"
-    ];
+    $host = "localhost";
+    $db = "saludos_db";
+    $user = "sofia";
+    $pass = "Contraseña_$123!";
 
-    return $saludos[array_rand($saludos)];
+    $conn = new mysqli($host, $user, $pass, $db);
+
+    if ($conn->connect_error) {
+        return "Error de conexión con la base de datos 😢";
+    }
+
+    $sql = "SELECT texto FROM saludos ORDER BY RAND() LIMIT 1";
+    $result = $conn->query($sql);
+
+    if ($result && $row = $result->fetch_assoc()) {
+        $saludo = sprintf($row['texto'], htmlspecialchars($nombre));
+    } else {
+        $saludo = "¡Hola $nombre!";
+    }
+
+    $conn->close();
+    return $saludo;
 }
